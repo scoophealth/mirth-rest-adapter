002656d71de4837d9722afc62413f48b00e29c9a
Glen McCallum <glen.mccallum@nextinformatics.com>
2012-06-26 12:08:54 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MirthRestAdapter::Application.config.secret_token = '4210f120f952db3b25af4b85e0877a9f7a2929c71595d954591cee7bd49f29deaaf7c05a496fc55373f719093ed83722bd4885d5c53bcd73dec9b3fe6c3d8728'
