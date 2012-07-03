72decc3bef7aac385c7474dea438d44dfa597773
Glen McCallum <glen.mccallum@nextinformatics.com>
2012-07-03 13:08:45 -0700

    Updated heroku app address for the hQuery gateway.

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -11,7 +11,7 @@ class RecordsController < ApplicationController
 
       res = nil
 
-      url = URI.parse('http://young-water-9709.herokuapp.com/records/create')
+      url = URI.parse('http://query-gateway.herokuapp.com/records/create')
       File.open("temp_scoop_document.xml") do |xml|
         req = Net::HTTP::Post::Multipart.new url.path, "content" => UploadIO.new(xml, "text/xml", "temp_scoop_document.xml")
         res = Net::HTTP.start(url.host, url.port) do |http|
